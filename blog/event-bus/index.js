086567612194e9910eee844c7f1c9dd214b87cc8
Adel <htwr86@gmail.com>
2020-05-10 16:49:32 +0300

    event-bus service. pass event to posts service

---
+++ blog/event-bus/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+  const event = req.body;
+
+  await axios.post('http://localhost:4000/events', event);
+  console.log(event);
+  res.status(200).send(event);
+});
+
+app.listen(4005, () => console.log('listening port 4005'));
